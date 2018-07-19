f624ba3e110d02c547c414ca322fdfa929124696
hidemikawai <yu93_min@yahoo.co.jp>
2018-07-19 22:13:07 +0900

    StepAsideUnityChanプロジェクト　初めてのコミット
    Lesson6で学習したスクリプトに課題～発展課題までを追記。

---
+++ Assets/ItemDestroy.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemDestroy : MonoBehaviour {
+
+	private GameObject unitychan;
+
+	// Use this for initialization
+	void Start () {
+		this.unitychan = GameObject.Find ("unitychan");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float distans = this.unitychan.transform.position.z - this.transform.position.z;
+		if (distans >= 15) {
+			Destroy (this.gameObject);
+		}
+	}
+}
